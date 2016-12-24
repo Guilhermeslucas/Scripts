18ba86e3835cbf6b4cda985d11bff982e0f51ceb
Guilhermeslucas <guilherme.slucas@gmail.com>
2016-12-23 23:00:04 -0200

    changing

--- list_files.sh
+++ list_files.sh
@@ -3,7 +3,8 @@
 #this is a really simple script I had to make on the fly while I was
 #a teaching assistant on Digital Circuits Subject. I showed a Terminal
 #with this script running so the students could be sure that they did
-#the submission of the exercise correctly
+#the submission of the exercise correctly. Just to make my life easier
+#instead of, refreshing the page everytime someone asks me.
 
 while true; do
     ls;
