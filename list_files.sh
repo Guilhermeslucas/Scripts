7214ac9ab9f99db695fd3df93948cba5b695923c
Guilhermeslucas <guilherme.slucas@gmail.com>
2016-12-23 22:54:33 -0200

    adding list files

---
+++ list_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#this is a really simple script I had to make on the fly while I was
+#a teaching assistant on Digital Circuits Subject. I showed a Terminal
+#with this script running so the students could be sure that they did
+#the submission of the exercise correctly
+
+while true; do
+    ls;
+    sleep 5;
+    clear;
+done
